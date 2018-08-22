3d655babaaa509e7c5a8f9f8e3250fc049710de1
systemplado <systemplado@gmail.com>
2018-08-22 20:14:41 +0800

    simple/static font configuration

--- dmenu/dmenu.sh
+++ dmenu/dmenu.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-dmenu_conf="-m 0 -fn Input:size=9 -nb #000000 -nf #ffffff -sb #222222 -sf #ffffff"
+# TODO: dynamic font
+dmenu_conf="-m 0 -nb #000000 -nf #ffffff -sb #222222 -sf #ffffff -i"
 
 spawn_firefox() {
   choices="Clandestine\nMr Templado"
-  choice=$(echo -e $choices | dmenu -p "Firefox" $dmenu_conf)
+  choice=$(echo -e $choices | dmenu -p "Firefox" $dmenu_conf -fn "xos4 Terminus:size=9")
   [ ${#choice} -ne 0 ] && firefox -p "$choice" 2> ~/.dwm.log
 }
 
 run() {
-  dmenu_run $dmenu_conf
+  dmenu_run $dmenu_conf -fn "xos4 Terminus:size=9"
 }
 
 case $1 in
